430d5fe0833a22f098910bdc377bf79f469db779
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-12-21 07:48:07 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import { ThemedLandingPage } from './pages/LandingPage/LandingPage'
 import SignInPage from './pages/SignIn/SignIn'
 import RegisterPage from './pages/Register/Register'
+import SettingsPage from './pages/SettingsPage/SettingsPage';
 function App() {
   const theme = "dark";
   return (
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<ThemedLandingPage  />} />
           <Route path='/SignIn' element={<SignInPage/>}/>
           <Route path='/Register' element={<RegisterPage theme={theme}/>}/>
-
+          <Route path='/settings' element={<SettingsPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Form from '../../components/Form/Form';
+function SettingsPage() {
+    const formFields = [
+        {
+          name: "username",
+          type: "text",
+          required: false,
+          validate: (value) => value.length >= 3 || value === "", 
+          errorMessage: "Username should be at least 3 characters",
+        },
+        {
+          name: "email",
+          type: "email",
+          required: false, 
+          validate: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || value === "",
+          errorMessage: "Please enter a valid email address",
+        },
+        {
+          name: "Oldpassword",
+          type: "password",
+          required: false, 
+          validate: (value) => value.length >= 6 || value === "", 
+          errorMessage: "Password must be at least 6 characters long",
+        },
+        {
+          name: "newPassword",
+          type: "password",
+          required: false, 
+          validate: (value, formFields) => {
+            const oldPasswordField = formFields.find(field => field.name === "Oldpassword");
+            if (value && !oldPasswordField.value) {
+              return false;
+            }
+            return value.length >= 6 || value === ""; 
+          },
+          errorMessage: "New password must be at least 6 characters long and Old password cannot be empty if New password is filled",
+        },
+      ];
+      const handleSubmit = (data) => {
+        console.log("Form Submitted:", data);
+      };
+
+    return (
+        <div>
+            <div>
+                <h1>Settings</h1>
+            </div>        
+            <Form fields={formFields} onSubmit={handleSubmit} buttonLabel={"Update"}/>
+            <div>
+                <button>Log out</button>
+            </div>
+        </div>
+    );
+}
+
+export default SettingsPage;
