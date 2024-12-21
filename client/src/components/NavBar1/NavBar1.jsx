854b66fb97c8fe98aeeb6f75db45f83b5eae49ac
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-12-21 07:56:09 +0530

    commit

---
+++ client/src/components/NavBar1/NavBar1.jsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+import React from 'react'
+
+function NavBar1() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default NavBar1
