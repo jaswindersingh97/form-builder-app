95c30f6cc1d254477eab8b367121d229838d2a95
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-12-26 19:59:51 +0530

    ok

--- client/src/components/NavBar1/NavBar1.jsx
+++ client/src/components/NavBar1/NavBar1.jsx
@@ -4,19 +4,30 @@ import UserMenu from '../UserMenu/UserMenu';
 import styles from './index.module.css';
 import React from 'react'
 import EmailInvites from './../WorkSpaceModals/EmailInvites/EmailInvites';
+import Dropdown from '../Dropdown/Dropdown';
+import {Link, useNavigate} from 'react-router-dom'
 function NavBar1() {
   const {openModal,closeModal} = useModal();
-
-
-
-
+  const navigate = useNavigate()
   const ShareClk = () =>{
     openModal(<EmailInvites/>);
+  }
+  const Logout = () =>{
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    navigate("/");
   }
   return (
     <div className={styles.container}>
       <div className={styles.middle}>
-        <UserMenu/>
+        <Dropdown>
+        <Link to={'/settings'}><div className={styles.menuItem}>Settings</div></Link>
+            <div onClick={Logout} className={styles.menuItem}>Logout</div>
+            <hr/>
+            <div className={styles.menuItem}>edit Rights</div>
+            <hr/>
+            <div className={styles.menuItem}>view Rights</div>
+        </Dropdown>
       </div>
       <div className={styles.right}>
         <ToggleButton/>
