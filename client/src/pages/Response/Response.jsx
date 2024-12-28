0145b304f9322683fb5f65cc98ef02ac99905edb
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-12-28 23:05:16 +0530

    commit

--- client/src/pages/Response/Response.jsx
+++ client/src/pages/Response/Response.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import withTheme from "../../components/ThemeComponent/ThemeComponent";
 import { useParams } from 'react-router-dom';
 import Api from '../../Api/Api';
-
+import styles from './styles.module.css'
 function Response() {
     const {formId} = useParams();
     const fetchData = async() =>{
@@ -17,8 +17,17 @@ function Response() {
         fetchData();
     },[])
   return (
-    <div class>
-
+    <div className={styles.container}>
+        <div className={styles.header}>
+            <div className={styles.count}><p>Views:</p> {}</div>
+            <div className={styles.count}><p>Starts:</p> {}</div>
+        </div>
+        <div className={styles.body}>
+            
+        </div>
+        <div className={styles.footer}>
+            
+        </div>
     </div>
   )
 }
